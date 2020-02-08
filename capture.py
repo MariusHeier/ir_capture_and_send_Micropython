8eb7069b1f57b5909b898efa99737c4e6c9c3c84
mariusheier <mariusheier1981@gmail.com>
2020-02-08 08:54:58 +0100

    tiny cleanup

--- capture.py
+++ capture.py
@@ -3,41 +3,43 @@ from machine import Pin
 
 
 def get_binary():
-    num1s = 0
-    binary = 1
-    command = []
-    previous_value = 0
-    ir_input = Pin(21, Pin.IN)
+    try:
+        num1s = 0
+        binary = 1
+        command = []
+        previous_value = 1
+        ir_input = Pin(21, Pin.IN)
 
-    while ir_input.value():
-        value = ir_input.value()
-        start_time = utime.ticks_us() 
+        while ir_input.value():
+            value = ir_input.value()
+            start_time = utime.ticks_us() 
 
-    while True:
-        if previous_value != value:
-            now = utime.ticks_us()
-            pulse_time = utime.ticks_diff(now, start_time)
-            start_time = now
-            command.append((previous_value, pulse_time))
-        
-        if value:
-            num1s += 1
-        else:
-            num1s = 0
-        if num1s > 1000:
-            break
-        previous_value = value
-        value = ir_input.value()
-    for (typ, tme) in command:
-        if typ == 1:
-            if tme > 1000:
-                binary = binary * 10 +1
+        while True:
+            if previous_value != value:
+                now = utime.ticks_us()
+                pulse_time = utime.ticks_diff(now, start_time)
+                start_time = now
+                command.append((previous_value, pulse_time))
+            previous_value = value
+            value = ir_input.value()
+            if value:
+                num1s += 1
             else:
-                binary *= 10
-        print(binary)
-
-    print(command)
-    return binary
+                num1s = 0
+            if num1s > 1000:
+                break
+        if len(command) > 10:
+            for (typ, tme) in command:
+                if typ == 1:
+                    if tme > 1000:
+                        binary = binary * 10 +1
+                    else:
+                        binary *= 10
+            print(binary)
+        return binary
+    except Exception as e:
+        print(e)
+        return None
 
 def convert_hex(binary_value):
     tmpB2 = int(str(binary_value), 2)
@@ -45,9 +47,11 @@ def convert_hex(binary_value):
 
 while True:
     binary_data = get_binary()
-    hex_data = convert_hex(binary_data)
-    if len(str(binary_data)) > 10:
-        print("Hex:", hex_data)
-        print("Binary:", binary_data)
-        print("Binary length:",len(str(binary_data)))
-        print("\n")
+    
+    if binary_data:        
+        if len(str(binary_data)) > 10:
+            hex_data = convert_hex(binary_data)
+            print("Hex:", hex_data)
+            print("Binary:", binary_data)
+            print("Binary length:",len(str(binary_data)))
+            print("\n")
